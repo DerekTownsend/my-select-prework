6161864877d470a17c20875b5bf24d745a28b80b
Derek Townsend <derekjavon2@gmail.com>
2019-05-09 05:51:46 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,10 +2,12 @@ def my_select(collection)
  # your code here!
  if block_given?
    i=0
+   temp=[]
    newCollection=[]
    while i<collection.length
      newCollection<<yield(collection[i])
      i+=1
+
    end
    newCollection
  else
